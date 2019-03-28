4cc2ab3f60feea7fe1fa56640d30be14aeb59af3
Korey Kinney <kkinney2@student.fitchburgstate.edu>
2019-03-28 11:28:01 -0400

    kkinney_MissedPush
    Merge Due to Missed Push

---
+++ kkinney_TeamGame1_21Mar19/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rb;
+
+    float playerInputX;
+    float playerInputY;
+    Vector3 m_Velocity = Vector3.zero;
+    float m_MovementSmoothing = .05f;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	void Update () {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Movement(moveHorizontal * Time.deltaTime, moveVertical * Time.deltaTime);
+	}
+
+    void Movement(float moveX, float moveZ)
+    {
+        Vector3 targetVelocity = new Vector3(moveX, 0.0f, moveZ);
+        //targetVelocity = moveTarget.transform.TransformVector(targetVelocity) * speed;
+
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+        //rb.AddForce(targetVelocity * speed);
+    }
+}
