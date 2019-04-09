d2081a3187693dba44ed22118bcf8b312a39a298
Kevin H <khickso1@student.fitchburgstate.edu>
2019-04-09 03:20:03 -0400

    Added script for second puzzle, made box easier to move.

---
+++ kkinney_TeamGame1_21Mar19/Assets/Scripts/defaultPhysicsMaterial.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class defaultPhysicsMaterial : MonoBehaviour {
+
+    public PhysicMaterial physicMaterial;
+
+	void Start () {
+        Transform[] allObjects = FindObjectsOfType<Transform>();
+        foreach (Transform t in allObjects)
+        {
+            if (t.GetComponent<MeshCollider>() != null)
+            {
+                t.GetComponent<MeshCollider>().material = physicMaterial;
+            }
+        }
+	}
+}
